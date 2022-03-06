198522dc73353be7207773178af0eb24fb4f6018
littlereed <lookluwei@gmail.com>
2022-03-06 10:46:55 +0800

    first commit

---
+++ next.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    images: {
+      domains: ['links.papareact.com'],   
+    },
+    env: {
+      mapbox_key: 'pk.eyJ1IjoibXlib29rMjAyMiIsImEiOiJja3RvYWdkdXcwYTFuMnBwMzUyYXhyOHNzIn0.irH6PGOrA0xf-QvT_32vPw'
+    }
+  };
